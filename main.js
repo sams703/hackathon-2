5b90099412f0fd54e709fb2966c76e70a6c612a4
Samson Saeed <kukusaeed1996@gmail.com>
2025-09-01 03:19:11 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+// Hamburger toggle
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  navMenu.classList.toggle("active");
+});
+
+// Close menu on nav-link click
+document.querySelectorAll(".nav-link").forEach(link =>
+  link.addEventListener("click", () => {
+    navMenu.classList.remove("active");
+  })
+);
+
+// Smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth"
+    });
+  });
+});
